2094b3ec48385a28612b66cd63b8c7eeafa0e4ba
FMarquesPTO <55203647+FMarquesPTO@users.noreply.github.com>
2020-12-23 17:00:45 +0000

    some updates

---
+++ ui/play_screen.json.d
@@ -0,0 +1,23 @@
+{
+  "play_screen@screen_jexp.screen": {
+    "button_mappings": [
+      { "from_button_id": "button.menu_cancel",
+        "to_button_id": "button.menu_exit",
+        "mapping_type": "global"
+      },
+      { "from_button_id": "button.menu_tab_left",
+        "to_button_id": "button.menu_tab_left",
+        "mapping_type": "global",
+        "scope": "view"
+      },
+      { "from_button_id": "button.menu_tab_right",
+        "to_button_id": "button.menu_tab_right",
+        "mapping_type": "global",
+        "scope": "view"
+      }
+    ],
+    "$scrn_cont": "play_jexp.screen_content",
+    "$scrn_bg": "screen_jexp.dirt_background",
+    "$world_picker_screen": false
+  }
+}
